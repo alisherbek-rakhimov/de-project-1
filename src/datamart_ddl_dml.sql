b2438954bfc64f099021c6d43ab4d22a3a2bf096
Alisherbek Rakhimov <alisherrakhimov1997@gmail.com>
2022-04-27 11:41:02 +0500

    left join

--- src/datamart_ddl_dml.sql
+++ src/datamart_ddl_dml.sql
@@ -1,6 +1,6 @@
-DROP TABLE if exists dm_rfm_segments;
+DROP TABLE if exists analysis.dm_rfm_segments;
 
-create table dm_rfm_segments
+create table analysis.dm_rfm_segments
 (
     user_id        int primary key,
     recency        int check ( recency in (1, 2, 3, 4, 5) ),
@@ -14,32 +14,25 @@ with gte_2021_closed_orders as (select o.order_id, user_id, payment, order_ts
                                          join analysis.orders o on ol.order_id = o.order_id
                                 where status_id = 4
                                   and extract(year from order_ts) >= 2021),
-     gte_2021_closed_orders_with_no_order_users as (
-         -- Тут нужно отобрать именно юзеров с успешными заказами и тех которые вообще ничего не закали
-         -- и нужно исключить тех кто заказал но еще не Closed для расчета RFM
-         select o.order_id, u.id as user_id, payment, order_ts
-         from analysis.users u
-                  left join analysis.orders o on u.id = o.user_id
-         where order_id is null
-         union all
-         select *
-         from gte_2021_closed_orders),
-     u_recency as (select user_id as user_id, max(order_ts) last_order_was
-                   from gte_2021_closed_orders_with_no_order_users o
-                   group by user_id),
-     u_frequency as (select user_id, count(order_id) orders_cnt
-                     from gte_2021_closed_orders_with_no_order_users o
-                     group by user_id),
-     u_monetary as (select user_id as user_id, sum(payment) spent
-                    from gte_2021_closed_orders_with_no_order_users o
-                    group by user_id)
-select ur.user_id,
---        spent,
-       ntile(5) over (order by spent)               as monetary_value,
+     u_recency as (select u.id as user_id, max(order_ts) last_order_was
+                   from gte_2021_closed_orders o
+                            right join analysis.users u on o.user_id = u.id
+                   group by u.id),
+     u_frequency as (select u.id as user_id, count(order_id) orders_cnt
+                     from gte_2021_closed_orders o
+                              join analysis.users u on o.user_id = u.id
+                     group by u.id),
+     u_monetary as (select u.id as user_id, sum(payment) spent
+                    from gte_2021_closed_orders o
+                             join analysis.users u on o.user_id = u.id
+                    group by u.id)
+select u_recency.user_id,
 --        last_order_was,
        ntile(5) over (order by last_order_was desc) as recency,
 --        orders_cnt,
-       ntile(5) over (order by orders_cnt)          as frequency
-from u_recency ur
-         join u_frequency uf on ur.user_id = uf.user_id
-         join u_monetary um on ur.user_id = um.user_id;
+       ntile(5) over (order by orders_cnt)          as frequency,
+--        spent,
+       ntile(5) over (order by spent)               as monetary_value
+from u_recency
+       left join u_frequency on u_recency.user_id = u_frequency.user_id
+       left join u_monetary on u_recency.user_id = u_monetary.user_id
--- src/views_ddl.sql
+++ src/views_ddl.sql
@@ -3,27 +3,27 @@ create view analysis.users as
 select *
 from production.users;
 
-DROP view if exists analysis.users;
+DROP view if exists analysis.orders;
 create view analysis.orders as
 select *
 from production.orders;
 
-DROP view if exists analysis.users;
+DROP view if exists analysis.orderstatuses;
 create view analysis.orderstatuses as
 select *
 from production.orderstatuses;
 
-DROP view if exists analysis.users;
+DROP view if exists analysis.products;
 create view analysis.products as
 select *
 from production.products;
 
-DROP view if exists analysis.users;
+DROP view if exists analysis.orderstatuslog;
 create view analysis.orderstatuslog as
 select *
 from production.orderstatuslog;
 
-DROP view if exists analysis.users;
+DROP view if exists analysis.orderitems;
 create view analysis.orderitems as
 select *
 from production.orderitems;
